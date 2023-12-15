38ff3ef55f07024968fe9d7064b27d4d5b7f9925
@DouglasECP <douglas_elite@hotmail.com>
2023-12-15 11:09:02 -0300

    Envio do projeto de cronometro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './style.css'
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            numero:0,
+            txtBtn: 'VAI'
+        }
+        this.timer = null; //Recebe a hora que vai aparecer no cronometro
+        this.vai = this.vai.bind(this);
+        this.limpar = this.limpar.bind(this);
+    }
+
+    vai(){
+
+        let state = this.state;
+
+        if(this.timer !== null){ //função para pausar o cronometro
+            clearInterval(this.timer);
+            this.timer = null;
+            state.txtBtn = 'VAI'
+        }else{ //função para iniciar o cronometro/despausar
+            this.timer = setInterval(()=>{
+                let copState = this.state;
+                copState.numero += 0.1;
+                this.setState(copState);
+            },100)
+            state.txtBtn = 'PAUSAR'
+        }
+
+        this.setState(state);
+
+    }
+
+    limpar(){
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+
+        let copState = this.state;
+        copState.numero = 0;
+        copState.txtBtn = 'VAI';
+        this.setState(copState);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <img src={require('./assets/cronometro.png')} className="img"/>
+                <a className="timer">{this.state.numero.toFixed(1)}</a>
+                
+                <div className="areaBtn">
+                    <a className="botao" onClick={this.vai}>{this.state.txtBtn}</a>
+                    <a className="botao" onClick={this.limpar}>LIMPAR</a>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
